4338273ad554f17bf3de71b89e2d4e5eeb257367
Jun <gjuoun@gmail.com>
2018-06-15 05:14:41 -0300

    add class : Course.class, Section.class read database and generate courseList in CourseLists read dropdown selection and pass it to courseList.class in RegisterClass

--- app/src/main/java/com/example/t/groupproject/Section.java
+++ app/src/main/java/com/example/t/groupproject/Section.java
@@ -1,29 +1,49 @@
 package com.example.t.groupproject;
 
 public class Section {
-    private int sectionId;
-    private int courseId;
+    private String sectionCode;
+    private String sectionType;
+    private String courseId;
     private String location;
+    private String times;
+    private String currentSeat;
+    private String availableSeat;
+    private String maxSeat;
+    private String waitList;
 
-    public Section(int sectionId, int courseId, String location){
+    public Section(String sectionCode, String sectionType, String courseId, String location, String times, String currentSeat, String availableSeat, String maxSeat, String waitList) {
+        this.sectionCode = sectionCode;
+        this.sectionType = sectionType;
         this.courseId = courseId;
-        this.sectionId = sectionId;
         this.location = location;
+        this.times = times;
+        this.currentSeat = currentSeat;
+        this.availableSeat = availableSeat;
+        this.maxSeat = maxSeat;
+        this.waitList = waitList;
     }
 
-    public int getSectionId() {
-        return sectionId;
+    public String getSectionCode() {
+        return sectionCode;
     }
 
-    public void setSectionId(int sectionId) {
-        this.sectionId = sectionId;
+    public void setSectionCode(String sectionCode) {
+        this.sectionCode = sectionCode;
     }
 
-    public int getCourseId() {
+    public String getSectionType() {
+        return sectionType;
+    }
+
+    public void setSectionType(String sectionType) {
+        this.sectionType = sectionType;
+    }
+
+    public String getCourseId() {
         return courseId;
     }
 
-    public void setCourseId(int courseId) {
+    public void setCourseId(String courseId) {
         this.courseId = courseId;
     }
 
@@ -35,5 +55,43 @@ public class Section {
         this.location = location;
     }
 
+    public String getTimes() {
+        return times;
+    }
 
+    public void setTimes(String times) {
+        this.times = times;
+    }
+
+    public String getCurrentSeat() {
+        return currentSeat;
+    }
+
+    public void setCurrentSeat(String currentSeat) {
+        this.currentSeat = currentSeat;
+    }
+
+    public String getAvailableSeat() {
+        return availableSeat;
+    }
+
+    public void setAvailableSeat(String availableSeat) {
+        this.availableSeat = availableSeat;
+    }
+
+    public String getMaxSeat() {
+        return maxSeat;
+    }
+
+    public void setMaxSeat(String maxSeat) {
+        this.maxSeat = maxSeat;
+    }
+
+    public String getWaitList() {
+        return waitList;
+    }
+
+    public void setWaitList(String waitList) {
+        this.waitList = waitList;
+    }
 }
