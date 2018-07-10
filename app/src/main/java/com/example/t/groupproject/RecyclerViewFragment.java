f75b9cb86da0b73b690889666ab0283bb99f0ce9
Jun <gjuoun@gmail.com>
2018-07-10 18:10:40 -0300

    fragment saved point

---
+++ app/src/main/java/com/example/t/groupproject/RecyclerViewFragment.java
@@ -0,0 +1,18 @@
+package com.example.t.groupproject;
+
+import android.app.Fragment;
+
+public class RecyclerViewFragment extends Fragment{
+
+    private static final String TAG = "RecyclerViewFragment";
+    private static final String KEY_LAYOUT_MANAGER = "layoutManager";
+    private static final int SPAN_COUNT = 2;
+    private static final int DATASET_COUNT = 60;
+
+    private enum LayoutManagerType {
+        GRID_LAYOUT_MANAGER,
+        LINEAR_LAYOUT_MANAGER
+    }
+    
+
+}
